2cc2268d1fac050e11fc7d6fb018664a3fcb2f4b
warsantos <weltonarsantos@gmail.com>
2019-06-19 22:22:42 -0300

    MERGE master com jardel.

--- minimos_quadrados.py
+++ minimos_quadrados.py
@@ -5,5 +5,5 @@ import numpy as np
 from sys import argv, exit
 
 if __name__=='__main__':
-	data_frame = base.carregar_base(argv[1], base.colunas)
+	data_frame = base.carregar_base(argv[1], colunas)
 	print(data_frame.head())
