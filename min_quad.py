d4fc63df3cc22f21d6b3afd54cdad9ceb511ae76
warsantos <weltonarsantos@gmail.com>
2019-06-21 18:58:31 -0300

    Adiçao de codigo generico para calcular o sismtea de minimos qudrados.

--- min_quad.py
+++ min_quad.py
@@ -5,24 +5,24 @@ import pandas as pd
 import numpy as np
 from sys import argv, exit
 
-def quad(valor):
-	return valor*valor
-
 # Funções para calcular a regressão linear.
 
-def funcao_reta(valor):
+def funcao_reta_a1(valor):
 	return valor
 
-def funcao_reta_final(coef_a, coef_b, x_valores, y_valores):
+def funcao_reta_a2(valor):
+	return 1
+
+def reta_aprox(valores_x, valores_y, coef_a1, coef_a2):
 
 	# Calculando a regressão com suporte da função ajustada.
-	regressao = [ (coef_a * x + coef_b) for x in x_valores ]
-	grafico.distribuicao_aprox(x_valores, y_valores, regressao)
+	regressao = [ (coef_a1 * x + coef_a2) for x in valores_x ]
+	grafico.distribuicao_aprox(valores_x, valores_y, regressao)
 
 # Funções para calcular a aproximação parabólica.
 
 def parabola_a1(valor):
-	return math.pow(valor - 172, 2)
+	return math.pow(valor - 182, 2)
 	
 def parabola_a2(valor):
 	return valor
@@ -35,15 +35,72 @@ def parab_aprox(valores_x, valores_y, coef_a1, coef_a2):
 		res.append(v)
 	grafico.distribuicao_aprox(valores_x, valores_y, res)
 
+# Vetores com funções indexadas.
 
+# [0]: funções para cálculo com reta.
+# [1]: funções de calculo com parábola.
+funcoes = [[funcao_reta_a1, funcao_reta_a2], \
+	[parabola_a1, parabola_a2]]
 
+funcoes_aprox = [reta_aprox,\
+	parab_aprox]
 
-def preproc(data_frame, x_label, y_label):
-	y_valores = data_frame[y_label]
-	numero_valores = len(y_valores)
-	x_valores = np.array(range(1, numero_valores+1))
-	return x_valores, y_valores
+# Matriz de calculo do sistema.
+def matriz_a(valores, funcoes):
+
+	# Pegando o número de coeficientes.
+	n_coefs = len(funcoes)
+	ma = np.zeros((n_coefs, n_coefs), dtype=float)
+
+	# Calculando matriz.
+	id_f = 0
+	while id_f < len(funcoes):
+		id_f2 = id_f
+		g1 = funcoes[id_f]
+		while id_f2 < len(funcoes):
+			g2 = funcoes[id_f2]
+			for v in valores:
+				r = g1(v) * g2(v)
+				ma[id_f][id_f2] += r
+			# Atualizando diagonal secundária e posições abaixo da matriz.
+			ma[id_f2][id_f] = ma[id_f][id_f2]
+			id_f2 += 1
+		id_f += 1
+
+	return ma
+
+# Vetor B de cálculo do sistema.
+def vetor_b(valores_x, valores_y, funcoes):
+
+	# Pegando o número de coeficientes.
+	n_coefs = len(funcoes)
+	vb = np.zeros((n_coefs, 1), dtype=float)
+	id_f = 0
+	while id_f < len(funcoes):
+		g = funcoes[id_f]
+		for v, y in zip(valores_x, valores_y):
+			vb[id_f] += y * g(v)
+		id_f += 1
+
+	return vb
+
+def preproc(data_frame, y_label, x_label='Date'):
+	valores_y = data_frame[y_label]
+	numero_valores = len(valores_y)
+	valores_x = np.array(range(1, numero_valores+1), dtype=float)
+	return valores_x, valores_y
+
+def min_quad(data_frame, y_label, aprox_id):
+
+	valores_x, valores_y = preproc(data_frame, y_label)
+	ma = matriz_a(valores_x, funcoes[aprox_id])
+	vb = vetor_b(valores_x, valores_y, funcoes[aprox_id])
+	# Resolvendo o sistema.
+	solucao = np.linalg.solve(ma, vb)
+	print("SOLUÇÃO: ", solucao)
+	ffinal = funcoes_aprox[aprox_id]
+	ffinal(valores_x, valores_y, solucao[0], solucao[1])
 
 if __name__=='__main__':
 	data_frame = base.carregar_base_individual(argv[1])
-	min_quad(data_frame, argv[2])
+	min_quad(data_frame, argv[2], int(argv[3]))
